c28fdb343c776ccbc34698969da133dfd94224d5
Samira <142447411+smasr008@users.noreply.github.com>
2025-11-21 16:31:18 -0500

    Fix issue with styling and images

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/probya-coming-soon' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/probya-coming-soon/' : '',
 }
 
 module.exports = nextConfig
