bbb6a6784f8dda6aa63988a650692f6a37dd8432
Samira <142447411+smasr008@users.noreply.github.com>
2025-11-21 17:02:55 -0500

    Fix Supabase URL

--- app/lib/supabase.js
+++ app/lib/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://mcgdnmmpoesrhtwrttvt.supabase.co'
+const supabaseUrl = 'https://mcgdmnmpossrhtwwttvt.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1jZ2Rtbm1wb3Nzcmh0d3d0dHZ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMzMTIzMTQsImV4cCI6MjA3ODg4ODMxNH0.2fUWckCbBZi_PbEA1aPHo6zWIrAdkzjQ6CliRejYW8Y'
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
