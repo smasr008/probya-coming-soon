fcf27a6f9f7e60c2f7ffe4bc3d4e3b868bac36e4
Samira <142447411+smasr008@users.noreply.github.com>
2025-11-21 16:24:46 -0500

    Updated json file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/probya-coming-soon' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/probya-coming-soon/' : '',
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
